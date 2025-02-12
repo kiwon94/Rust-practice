d802163ca1cb995c9c397bfeb7d02dfad98aea17
Keon <greenare@cluml.com>
2025-02-12 14:20:49 +0900

    Add a solve function for problem 15 (#31)
    Defines a function that splits an input sentence into words using
    split_whitespace(), extracts each word's position, and sorts the words
    based on their position. After sorting, the function reconstructs the
    sentence by joining the sorted words together. The function returns the
    newly sorted sentence as a single string.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod problem11;
 mod problem12;
 mod problem13;
 mod problem14;
+mod problem15;
 mod problem2;
 mod problem3;
 mod problem4;
@@ -41,6 +42,7 @@ fn main() {
         12 => problem12::solve(),
         13 => problem13::solve(),
         14 => problem14::solve(),
+        15 => problem15::solve(),
         _ => println!("Problem not found"),
     }
 }
---
+++ src/problem15.rs
@@ -0,0 +1,32 @@
+// Problem 15. Sorting the Sentence
+fn sort_sentence(s: &str) -> String {
+    let words: Vec<&str> = s.split_whitespace().collect();
+
+    let mut words_with_index: Vec<(usize, &str)> = words
+        .iter()
+        .map(|x| {
+            let index = x
+                .chars()
+                .last()
+                .expect("Each word has at aleast one character")
+                .to_digit(10)
+                .expect("Failed to convert char to digit") as usize;
+            let words_stripped = &x[..x.len() - 1];
+            (index, words_stripped)
+        })
+        .collect();
+
+    words_with_index.sort_unstable_by_key(|&(index, _)| index);
+    let result: String = words_with_index
+        .iter()
+        .map(|&(_, word)| word)
+        .collect::<Vec<&str>>()
+        .join(" ");
+    result
+}
+
+pub fn solve() {
+    let s: String = "is2 sentence4 This1 a3".to_string();
+    let result = sort_sentence(&s);
+    println!("Sorted sentence: {result}");
+}
