5aa69e370a8ea7659c11bb807c867f25aca5770a
Keon <greenare@cluml.com>
2025-02-13 17:09:45 +0900

    Refactoring a solve function of problem 16
    Change restore_string function to return Result<String, TryFromIntError>, which
    returns an Error on failure with try_from(). Modify solve function to
    print a successful result on Ok, and an error message on Err.

--- src/problem19.rs
+++ src/problem19.rs
@@ -1,22 +1,33 @@
 // Problem 19. Shuffle String
-fn restore_string(s: &str, indices: &[i32]) -> String {
+use std::num::TryFromIntError;
+
+fn restore_string(s: &str, indices: &[i32]) -> Result<String, TryFromIntError> {
     let mut chars: Vec<char> = s.chars().collect();
     let mut indices = indices.to_vec();
     let n = chars.len();
 
-    for i in 0..n {
-        while indices[i] != i32::try_from(i).expect("Failed to convert to i32") {
-            let j = indices[i];
-            chars.swap(i, usize::try_from(j).expect("Failed to convert to usize"));
-            indices.swap(i, usize::try_from(j).expect("Failed to convert to usize"));
+    for idx in 0..n {
+        let idx_as_i32 = i32::try_from(idx)?;
+        while indices[idx] != idx_as_i32 {
+            let v_idx = usize::try_from(indices[idx])?;
+            chars.swap(idx, v_idx);
+            indices.swap(idx, v_idx);
         }
     }
-    chars.iter().collect()
+
+    Ok(chars.iter().collect())
 }
 
 pub fn solve() {
     let s = "codeleet";
-    let indices = [4, 5, 6, 7, 0, 2, 1, 3];
-    let result = restore_string(s, &indices);
-    println!("Restored string: {result}");
+    let indices = [4, 5, 6, 7, 1, 2, 1, 3];
+    match restore_string(s, &indices) {
+        Ok(result) => {
+            println!("Shuffled result: {result}");
+        }
+
+        Err(e) => {
+            println!("Error: {e}");
+        }
+    }
 }
