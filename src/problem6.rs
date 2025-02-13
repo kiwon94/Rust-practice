26347bb194e1459a2a9f60d8533a56f03cb0837d
Keon <greenare@cluml.com>
2025-02-13 11:24:26 +0900

    Refactoring a solve function of problem 6 (#67)
    Change input format (2 variables to 1 variable). Use flat_map() method
    to make a shuffled array, instead of using for loop.

--- src/problem6.rs
+++ src/problem6.rs
@@ -1,30 +1,17 @@
 // Problem 6. Shuffle the Array
 
-fn shuffle(nums: &[i32], n: i32) -> Result<Vec<i32>, String> {
-    let n_usize: usize = match n.try_into() {
-        Ok(n_usize) => n_usize,
-        Err(_) => {
-            return Err("n must be a non-negative integer".to_string());
-        }
-    };
-
-    let left_half: &[i32] = &nums[..(n_usize)];
-    let right_half: &[i32] = &nums[(n_usize)..];
-    let mut shuffle_nums: Vec<i32> = Vec::new();
-    let zip: Vec<(&i32, &i32)> = left_half.iter().zip(right_half.iter()).collect();
-    for (l_num, r_num) in zip {
-        shuffle_nums.push(*l_num);
-        shuffle_nums.push(*r_num);
-    }
-    Ok(shuffle_nums)
+fn shuffle(nums: &[i32]) -> Vec<i32> {
+    let n = nums.len() / 2;
+    nums[..n]
+        .iter()
+        .zip(&nums[n..])
+        .flat_map(|(&x, &y)| vec![x, y])
+        .collect()
 }
 
 pub fn solve() {
     // Solution for Problem 6
     let nums: Vec<i32> = vec![2, 5, 1, 3, 4, 7];
-    let n: i32 = 3;
-    match shuffle(&nums, n) {
-        Ok(shuffled) => println!("Shuffled: {shuffled:?}"),
-        Err(e) => println!("Error: {e}"),
-    }
+    let result = shuffle(&nums);
+    println!("Shuffled array: {result:?}");
 }
