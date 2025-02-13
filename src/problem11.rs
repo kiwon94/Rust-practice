d7f7cab5ef46ed14bbd86f5ab92a57a058f36fe4
Keon <greenare@cluml.com>
2025-02-13 15:57:20 +0900

    Refactoring a solve function of problem 11 (#69)
    Use a iter() method with a filter() and a count() method to check how
    many numbers are smaller than the current number while iterating through
    another iter() method.

--- src/problem11.rs
+++ src/problem11.rs
@@ -1,20 +1,8 @@
 // Problem 11. How Many Numbers Are Smaller Than the Current Number
-use std::collections::HashMap;
-fn smaller_numbers_than_current(nums: &[i32]) -> Vec<i32> {
-    let mut sorted_nums: Vec<i32> = nums.to_vec();
-    sorted_nums.sort_unstable();
-    let mut rank_map = HashMap::new();
-    for (idx, &val) in sorted_nums.iter().enumerate() {
-        rank_map
-            .entry(val)
-            .or_insert(i32::try_from(idx).expect("Index is not a valid i32"));
-    }
-    let result: Vec<i32> = nums
-        .iter()
-        .map(|&x| *rank_map.get(&x).expect("The value must  exist in the map"))
-        .collect();
-
-    result
+fn smaller_numbers_than_current(nums: &[i32]) -> Vec<usize> {
+    nums.iter()
+        .map(|&x| nums.iter().filter(|&&y| y < x).count())
+        .collect()
 }
 
 pub fn solve() {
