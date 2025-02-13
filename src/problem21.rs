7b8310f4cd19666a050d8f3e7e68ab427b167188
Keon <greenare@cluml.com>
2025-02-13 17:58:41 +0900

    Refactoring a solve function of problem 21
    Refactored get_decimal_value to use Vec<i32> as input instead of
    ListNode. Use Bitmask operation to calculate the binary
    number. Removed commented-out code.

--- src/problem21.rs
+++ src/problem21.rs
@@ -1,40 +1,11 @@
 // Problem 21 - Convert Binary Number in a Linked List to Integer
 
-// Definition for singly-linked list.
-#[derive(PartialEq, Eq, Clone, Debug)]
-pub struct ListNode {
-    pub val: i32,
-    pub next: Option<Box<ListNode>>,
-}
-
-// impl ListNode {
-//     #[inline]
-//     fn new(val: i32) -> Self {
-//         ListNode { next: None, val }
-//     }
-// }
-
-fn get_decimal_value(head: Option<Box<ListNode>>) -> i32 {
-    let mut binary_digits = Vec::new();
-    let mut current = head;
-
-    while let Some(node) = current {
-        binary_digits.push(node.val.to_string());
-        current = node.next;
-    }
-
-    let binary_str = binary_digits.join("");
-    i32::from_str_radix(&binary_str, 2).expect("Failed to convert binary string to integer")
+fn get_decimal_value(input: &[i32]) -> i32 {
+    input.iter().fold(0, |acc, &bit| (acc << 1) | bit)
 }
 
 pub fn solve() {
-    let head = Some(Box::new(ListNode {
-        val: 1,
-        next: Some(Box::new(ListNode {
-            val: 0,
-            next: Some(Box::new(ListNode { val: 1, next: None })),
-        })),
-    }));
-    let output = get_decimal_value(head);
+    let input = vec![1, 0, 1];
+    let output = get_decimal_value(&input);
     println!("Integer value: {output}");
 }
