dafaa63e451ed39e541f9d428121a580cb32a1b8
Keon <greenare@cluml.com>
2025-02-13 11:56:12 +0900

    Refactoring a solve function of problem 13
    Use filter(), count(), contains() method to find how many stones are
    contained in the jewels. Return with Option<i32> type to check if
    there's an error.

--- src/problem13.rs
+++ src/problem13.rs
@@ -1,22 +1,16 @@
-// Problem 12. Defanging an IP Address
-use std::collections::HashSet;
-
-fn num_jewels_in_stones(jewels: &str, stones: &str) -> i32 {
-    let jewel_set: HashSet<char> = jewels.chars().collect();
-    let mut jewel_cnt = 0;
-
-    for stone in stones.chars() {
-        if jewel_set.contains(&stone) {
-            jewel_cnt += 1;
-        }
-    }
-
-    jewel_cnt
+// Problem 13. Jewels and Stones
+fn num_jewels_in_stones(jewels: &str, stones: &str) -> Option<i32> {
+    let jewel_count = stones
+        .chars()
+        .filter(|&stone| jewels.contains(stone))
+        .count();
+    i32::try_from(jewel_count).ok()
 }
-
 pub fn solve() {
     let jewels = "aA";
     let stones = "aAAbbbb";
-    let result = num_jewels_in_stones(jewels, stones);
-    println!("Number of jewels in stones: {result}");
+    match num_jewels_in_stones(jewels, stones) {
+        Some(count) => println!("Number of jewels in stones: {count}"),
+        None => println!("Failed to convert the count value to i32"),
+    }
 }
